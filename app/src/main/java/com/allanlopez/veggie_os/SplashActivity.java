d8a723be71c27e4ec67ec16245d87f15ffc96e16
allanloji <allanloji@gmail.com>
2018-03-01 20:51:41 -0600

    Menu creado

--- app/src/main/java/com/allanlopez/veggie_os/SplashActivity.java
+++ app/src/main/java/com/allanlopez/veggie_os/SplashActivity.java
@@ -14,7 +14,7 @@ public class SplashActivity extends Activity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent it = new Intent( SplashActivity.this, MainActivity.class);
+                Intent it = new Intent( SplashActivity.this, MenuActivity.class);
                 startActivity(it);
                 finish();
             }
